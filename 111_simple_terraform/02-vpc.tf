2b02f0d8ef8aa3d498ef9337baccf4bcf33f1719
Andy Golubev <30592974+andygolubev@users.noreply.github.com>
2025-10-08 16:55:26 +0300

    Update VPC configuration to enhance subnet CIDR blocks and add NAT gateway resources
    - Adjust CIDR block calculations for private and public subnets to accommodate additional IP addresses.
    - Introduce NAT gateway and associated Elastic IP for outbound internet access from private subnets.
    - Create route tables for both public and private subnets, ensuring proper routing configurations.
    - Establish route table associations for subnets to facilitate traffic management.

--- 111_simple_terraform/02-vpc.tf
+++ 111_simple_terraform/02-vpc.tf
@@ -20,7 +20,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "private_subnet" {
   for_each          = toset(local.first_3_azs)
   vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 4, index(local.first_3_azs, each.value))
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 5, index(local.first_3_azs, each.value))
   availability_zone = each.value
 
   tags = {
@@ -33,7 +33,7 @@ resource "aws_subnet" "private_subnet" {
 resource "aws_subnet" "public_subnet" {
   for_each          = toset(local.first_3_azs)
   vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 4, index(local.first_3_azs, each.value) + 3)
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 5, index(local.first_3_azs, each.value) + 3)
   availability_zone = each.value
 
   tags = {
@@ -41,4 +41,63 @@ resource "aws_subnet" "public_subnet" {
     "kubernetes.io/role/elb"                              = "1"
     "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
   }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "nat-eip-${local.env}"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet[local.first_3_azs[0]].id
+
+  tags = {
+    Name = "nat-${local.env}"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "private-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "private-rt-${local.env}"
+  }
+}
+
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt-${local.env}"
+  }
+}
+
+resource "aws_route_table_association" "private-rt-association" {
+  for_each = aws_subnet.private_subnet
+  subnet_id = each.value.id
+  route_table_id = aws_route_table.private-rt.id
+
+}
+
+resource "aws_route_table_association" "public-rt-association" {
+  for_each = aws_subnet.public_subnet
+  subnet_id = each.value.id
+  route_table_id = aws_route_table.public-rt.id
+
 }
