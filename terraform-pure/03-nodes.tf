0d7708063ce4ebeae1cebf50125aa96a651d322d
Andy Golubev <30592974+andygolubev@users.noreply.github.com>
2026-01-12 14:03:33 +0200

    Enhance EKS node group configurations in Terraform
    - Renamed existing EKS node group to support ARM64 architecture and updated associated labels and tags.
    - Added a new EKS node group for AMD64 architecture with specific scaling and update configurations.
    - Implemented lifecycle management to ignore changes in desired size for the ARM64 node group.

--- terraform-pure/03-nodes.tf
+++ terraform-pure/03-nodes.tf
@@ -29,10 +29,10 @@ resource "aws_iam_role_policy_attachment" "eks_ec2_container_registry_read_only"
   role       = aws_iam_role.eks_node_role.name
 }
 
-resource "aws_eks_node_group" "eks_node_group_main" {
+resource "aws_eks_node_group" "eks_node_group_main_arm64" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   version         = var.kubernetes_version
-  node_group_name = "group-main"
+  node_group_name = "group-main-arm64"
 
   node_role_arn  = aws_iam_role.eks_node_role.arn
   subnet_ids     = values(aws_subnet.private)[*].id
@@ -52,10 +52,55 @@ resource "aws_eks_node_group" "eks_node_group_main" {
 
   labels = {
     role = "main"
+    arch = "arm64"
   }
 
   tags = {
-    Name = "eks-node-group-main"
+    Name = "eks-node-group-main-arm64"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      scaling_config[0].desired_size
+    ]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_ec2_container_registry_read_only,
+  ]
+
+}
+
+resource "aws_eks_node_group" "eks_node_group_secondary_amd64" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  version         = var.kubernetes_version
+  node_group_name = "group-secondary-amd64"
+
+  node_role_arn  = aws_iam_role.eks_node_role.arn
+  subnet_ids     = values(aws_subnet.private)[*].id
+  capacity_type  = "ON_DEMAND"
+  ami_type       = var.node_group_arch_and_instance_types.x86_64.ami_type
+  instance_types = var.node_group_arch_and_instance_types.x86_64.instance_types
+
+  scaling_config {
+    desired_size = var.node_desired_size
+    max_size     = var.node_max_size
+    min_size     = var.node_min_size
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  labels = {
+    role = "secondary"
+    arch = "amd64"
+  }
+
+  tags = {
+    Name = "eks-node-group-secondary-amd64"
   }
 
   lifecycle {
